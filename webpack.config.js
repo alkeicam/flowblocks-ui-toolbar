f4824bfb310642364794151e2c420991a92cd6f8
Maciej Grula <maciej.grula@execon.pl>
2020-04-03 01:10:31 +0200

    fixes

--- webpack.config.js
+++ webpack.config.js
@@ -7,33 +7,15 @@ module.exports = {
     entry: './src/toolbar.js',
     mode: 'production',
     output: {
-        filename: 'flowblocks-ui-toolbar.js', // <-- Important
+        filename: 'flowblocks-ui-toolbar.js', 
         path: path.resolve(__dirname, 'dist'),
-        // ,
-        // libraryTarget: 'this' // <-- Important
         library: 'flowblocksUiToolbar',
         libraryTarget: 'umd',
         globalObject: 'this'
     },
-    //target: 'node', // <-- Important
-    // module: {
-    //     rules: [
-    //         {
-    //             test: /\.tsx?$/,
-    //             //loader: 'ts-loader',
-    //             options: {
-    //                 //transpileOnly: true
-    //             }
-    //         }
-    //     ]
-    // },
+
     resolve: {
         extensions: [ '.js' ]
     },
     externals: {jointjs: "joint"}
-
-    // ,
-    // externals: [nodeExternals({
-    //     whitelist: ['paytip-module-tipbox','paytip-module-core', 'paytip-module-wallet', 'paytip-module-integration','paytip-module-integration-inbound','paytip-module-bo']
-    // })] // <-- Important
 };
